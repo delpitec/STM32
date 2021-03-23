e48111742778cfdce82756bd5959fe1ad210ea7d
DELPITEC by Rafael Del Pino <58537514+delpitec@users.noreply.github.com>
2021-03-23 09:20:05 -0300

    Update and rename lcd_v1.1.h to lcd_v1.2.h
    Local functions moved to .c file

--- STM32f103C8T6/Display LCD/lcd_v1.2.h
+++ STM32f103C8T6/Display LCD/lcd_v1.2.h
@@ -4,8 +4,6 @@ void lcd_escreve_instrucao_Init(char opcode);
 void lcd_escreve_instrucao_Init(char opcode);
 
 // Funções de utilização
-void lcd_escreve_dado(char dado);  		// escrever um caractere individualmente
-void lcd_escreve_instrucao(char instr); // escreve um dado de controle (clear display, pos. cursor, etc)
 void lcd_posicao_do_cursor(char lin, char col); // posição do cursor na tela
 void lcd_limpa_display(void);			// limpar o display + pos. l0 c0
 void lcd_escreve_string(char *c); 		// escreve uma string de dados de caracteres até receber \r
